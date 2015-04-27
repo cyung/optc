b0b8a7808ab5d0d536e798c5632434f3351f4947
Chris Yung <cyung@bu.edu>
2015-04-27 12:28:46 +0800

    added dynamic day for people in asia

--- js/app.js
+++ js/app.js
@@ -5,31 +5,37 @@ angular.module('app')
 		var self = this;
 		self.weekly_order = [0,1,2,3,4];
 		// self.weekly_order = [1,2,3,4,0]; // id=4 is the first time
-
 		self.timezone = jstz.determine().name();
 
+		self.utctime = moment.utc("2015-04-27 13:00");
+		self.my_offset = moment().utcOffset();
+		self.my_time = self.utctime.clone();
+		self.my_time.utcOffset(self.my_offset);
+
+		self.day = self.my_time.clone();
+
 		self.turtle = function turtle (id, second_time) {
-			var ttime = moment.utc("2015-04-27 13:00");
-			if (second_time)
-				ttime.add(10,'hours');
-			// set my_ttime1 to local time
-			var my_offset = moment().utcOffset();
-			var my_ttime = ttime.clone();
-			my_ttime.utcOffset(my_offset);
+			var ttime = self.my_time.clone();
 
 			// add additional offset for id 6th digit
 			if (id>4)
 				id -=5;
+			ttime.add(self.weekly_order[id]*2, 'hours');
 
-			my_ttime.add(self.weekly_order[id]*2, 'hours');
+			// if second turtle time, add 10 hours
+			if (second_time)
+				ttime.add(10,'hours');
+			else // if first time, set the day
+				self.day = ttime.clone();
 
-			return printTime(my_ttime);
+			return printTime(ttime);
 		};
 
 
-		function printDate(date) {
-			return date.format('YYYY-MM-DD HH:mm:ss');
-		}
+		self.getDate = function getDate(date) {
+			return self.day.format('MMMM Do, YYYY');
+		};
+
 		function printTime(date) {
 			return date.format('h:mm a');
 		}
