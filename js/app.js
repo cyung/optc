b37ec8031e5a57ca4b1a536542678b557133b4b9
Chris Yung <cyung@bu.edu>
2015-05-02 13:48:05 -0400

    added second week and changed layout

--- js/app.js
+++ js/app.js
@@ -3,37 +3,49 @@ angular.module('app', []);
 angular.module('app')
 	.controller('TimeCtrl', function(){
 		var self = this;
-		self.weekly_order = [4,0,1,2,3];
-		// self.weekly_order = [1,2,3,4,0]; // id=4 is the first time
+		self.weekly_order = [4,0,1,2,3]; // id=1 is the first offset
 		self.timezone = jstz.determine().name();
 
 		self.utctime = moment.utc("2015-05-04 13:00");
 		self.my_offset = moment().utcOffset();
 		self.my_time = self.utctime.clone();
 		self.my_time.utcOffset(self.my_offset);
-		self.day = self.my_time.clone();
+		self.day1 = self.my_time.clone();
+		self.day2 = self.my_time.clone();
 		self.military = false;
 
-		self.turtle = function turtle (id, second_time) {
+		self.turtle = function turtle (id, second_time, next_week) {
+			var weekly_order = self.weekly_order.slice(0);
 			var ttime = self.my_time.clone();
+			if (next_week){
+				ttime.add(7, 'days');
+				var temp = weekly_order.pop();
+				weekly_order.unshift(temp);
+			}
 
 			// add additional offset for id 6th digit
 			if (id>4)
 				id -=5;
-			ttime.add(self.weekly_order[id]*2, 'hours');
+			ttime.add(weekly_order[id]*2, 'hours');
 
 			// if second turtle time, add 10 hours
 			if (second_time)
 				ttime.add(10,'hours');
-			else // if first time, set the day
-				self.day = ttime.clone();
+			else {
+				if (!next_week)
+					self.day1 = ttime.clone();
+				else
+					self.day2 = ttime.clone();
+			}
 
 			return printTime(ttime);
 		};
 
 
-		self.getDate = function getDate(date) {
-			return self.day.format('MMMM Do, YYYY');
+		self.getDate = function getDate(next_week) {
+			if (!next_week)
+				return self.day1.format('MMMM Do, YYYY');
+			return self.day2.format('MMMM Do, YYYY');
 		};
 
 		function printTime(date) {
