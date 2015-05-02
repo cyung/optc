1bdc2db2f8aad3a74daeaa1726c4afe1a0d0348c
Chris Yung <cyung@bu.edu>
2015-05-02 12:15:54 -0400

    added 24hr toggle and updated the day

--- js/app.js
+++ js/app.js
@@ -3,16 +3,16 @@ angular.module('app', []);
 angular.module('app')
 	.controller('TimeCtrl', function(){
 		var self = this;
-		self.weekly_order = [0,1,2,3,4];
+		self.weekly_order = [4,0,1,2,3];
 		// self.weekly_order = [1,2,3,4,0]; // id=4 is the first time
 		self.timezone = jstz.determine().name();
 
-		self.utctime = moment.utc("2015-04-27 13:00");
+		self.utctime = moment.utc("2015-05-04 13:00");
 		self.my_offset = moment().utcOffset();
 		self.my_time = self.utctime.clone();
 		self.my_time.utcOffset(self.my_offset);
-
 		self.day = self.my_time.clone();
+		self.military = false;
 
 		self.turtle = function turtle (id, second_time) {
 			var ttime = self.my_time.clone();
@@ -37,6 +37,8 @@ angular.module('app')
 		};
 
 		function printTime(date) {
+			if (self.military)
+				return date.format('HH:mm');
 			return date.format('h:mm a');
 		}
 
