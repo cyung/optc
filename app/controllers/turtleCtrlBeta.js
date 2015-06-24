33e1c19ffbdc15d63fe9685a2e90a999754928e3
Chris Yung <cyung@bu.edu>
2015-06-24 09:10:33 -0400

    refactored code into services; added notification layout

---
+++ app/controllers/turtleCtrlBeta.js
@@ -0,0 +1,50 @@
+(function () {
+	'use strict';
+	angular.module('app')
+		.controller('TurtleCtrlBeta', turtleCtrlBeta);
+	function turtleCtrlBeta($scope, localStorageService, turtleFact) {
+		var self = this;
+		set_params();
+
+
+
+
+		function set_params() {
+			self.timezone = jstz.determine().name();
+			self.global = 'global';
+
+			self.id = localStorageService.get('id');
+			self.military = JSON.parse(localStorageService.get('military'));
+			self.global = localStorageService.get('version');
+			if (self.global === null)
+				self.global = 'global';
+		}
+
+		self.version = function() {
+			return (self.global === 'global');
+		};
+
+		self.range = function(num) {
+			return new Array(num);
+		};
+
+		$scope.$watch(function() {
+			return self.id;
+		}, function(newVal, oldVal) {
+			localStorageService.set('id', newVal);
+			turtleFact.set_id(self.id);
+			self.ttimes = turtleFact.get_ttimes();
+			console.log(self.ttimes);
+		});		
+		$scope.$watch(function() {
+			return self.military;
+		}, function(newVal, oldVal) {
+			localStorageService.set('military', newVal);
+		});		
+		$scope.$watch(function() {
+			return self.global;
+		}, function(newVal, oldVal) {
+			localStorageService.set('version', newVal);
+		});
+	}
+})();
---
+++ app/services/turtleFact.js
@@ -0,0 +1,73 @@
+(function () {
+	'use strict';
+
+	angular.module('app')
+		.factory('turtleFact', turtleFact);
+
+	function turtleFact() {
+		var ttimes = [];
+		var id;
+		var my_time = get_current_time();
+
+		var services = {
+			set_id: set_id,
+			get_ttimes: get_ttimes,
+			calc_ttimes: calc_ttimes_all
+		};
+
+		return services;
+
+		function set_id(i) {
+			id = i;
+		}
+
+		function get_ttimes() {
+			calc_ttimes_all();
+			return ttimes;
+		}
+
+		function get_current_time() {
+			var my_time = moment().utc().startOf('isoWeek').add(13, 'hours');
+			var end = my_time.clone().add(18, 'hours');
+			if (moment().isAfter(end))
+				my_time.add(1, 'week');
+			return my_time;
+		}
+
+		function calc_ttimes_all() {
+			var i = 0;
+			var second_time = false;
+			var num_weeks = 2;
+
+			while (i<num_weeks) {
+				ttimes.push(calc_ttime(i, second_time));
+				if (second_time)
+					i++;
+				second_time = !second_time;
+			}
+		}
+
+		function calc_ttime(week_num, second_time) {
+			var weekly_order = [0,1,2,3,4];
+			var ttime = my_time.clone();
+			var offset = week_num + my_time.isoWeek();
+
+			offset = offset % 5;
+
+			for (var i=0; i < offset; i++)
+				weekly_order.unshift(weekly_order.pop());
+
+			for (i=0; i < week_num; i++)
+				ttime.add(1, 'week');
+
+			if (id>4)
+				id -=5;
+
+			ttime.add(weekly_order[id]*2, 'hours');
+			if (second_time)
+				ttime.add(10,'hours');
+
+			return ttime;
+		}
+	}
+})();
