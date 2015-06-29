3099f20cdf1afeba257d36dce7195381ef4877ab
Chris Yung <cyung@bu.edu>
2015-06-28 21:25:39 -0400

    added beta directive

---
+++ app/directives/turtleBetaDirective.js
@@ -0,0 +1,55 @@
+(function() {
+	'use strict';
+
+	angular.module('app')
+	.directive('turtleBeta', 
+	turtle_beta);
+
+	function turtle_beta(turtleFact) {
+		return {
+			restrict: 'E',
+			scope: {
+				military: '=',
+				locale: '=',
+				index: '=',
+				numid: '='
+			},
+			template: '<div class="ttime-day"><p><span>{{date}}</span><span>{{ttime_format}}</span></p></div>',
+			link: linker
+		}
+
+		function linker(scope, elem, attrs) {
+			function update_times() {
+				var ttime = turtleFact.get_ttimes()[scope.index];
+				if (scope.locale !== null)
+					ttime.locale(scope.locale);
+
+				scope.date = ttime.format('LL');
+				scope.ttime_format = print_time(ttime);
+			}
+
+			function print_time (date) {
+				if (scope.military)
+					return date.format('HH:mm');
+				return date.format('h:mm a');
+			}
+
+
+			scope.$watch('numid', function(newVal, oldVal) {
+				update_times();
+			});
+
+			scope.$watch('military', function(newVal, oldVal) {
+				update_times();
+			});
+
+			scope.$watch('ttime', function(newVal, oldVal) {
+				update_times();
+			});
+
+			scope.$watch('locale', function(newVal, oldVal) {
+				update_times();
+			});
+		}
+	}
+})()
