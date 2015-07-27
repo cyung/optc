9d34659102bc7dd8df3bca65b37a66fdfe83fcfe
Chris Yung <cyung@bu.edu>
2015-07-27 18:57:07 -0400

    finished new implementation of JPN calculator

--- app/services/turtleFact.js
+++ app/services/turtleFact.js
@@ -6,11 +6,11 @@
 
 	function turtleFact() {
 		var ttimes = [];
-		var id;
-		var my_time = get_current_time();
+		var id, my_time;
 		var notifications = [];
 		var notif_time, notif_sound;
 		var notif_audio = new Audio('sounds/notification.mp3');
+		var version, jpn_monday;
 
 		var services = {
 			set_id: set_id,
@@ -18,28 +18,50 @@
 			get_cal: get_cal,
 			example_notification: example_notification,
 			set_notif_time: set_notif_time,
-			set_notif_sound: set_notif_sound
+			set_notif_sound: set_notif_sound,
+			set_version: set_version
 		};
 
 		return services;
 
 		function set_id(i) {
-			id = i;
+			id = i % 5;
 			get_ttimes();
 		}
 
 		function get_ttimes() {
+			get_current_time();
 			ttimes = [];
 			calc_ttimes_all();
 			return ttimes;
 		}
 
 		function get_current_time() {
-			var my_time = moment.utc().startOf('isoWeek').add(13, 'hours').local();
-			var end = my_time.clone().add(18, 'hours');
-			if (moment().isAfter(end))
-				my_time.add(1, 'week');
-			return my_time;
+			var end;
+			if (version === 'global') {
+				my_time = moment.utc().startOf('isoWeek').add(13, 'hours').local();
+				end = my_time.clone().add(18, 'hours');
+				if (moment().isAfter(end))
+					my_time.add(1, 'week');
+			}
+			else {
+				my_time = moment.utc().startOf('isoWeek');
+				jpn_monday = true;
+
+				var now = moment();
+				end = my_time.clone().add(18, 'hours');
+				
+				if (now.isAfter(end)) {
+					my_time.add(4, 'days');
+					end.add(4, 'days');
+					jpn_monday = false;
+				}
+
+				if (now.isAfter(end)) {
+					my_time.add(3, 'days');
+					jpn_monday = true;
+				}
+			}
 		}
 
 		function calc_ttimes_all() {
@@ -47,11 +69,18 @@
 			var i = 0;
 			var second_time = false;
 
+			if (version !== 'global')
+				num_weeks *= 2;
+
 			while (i<num_weeks) {
 				ttimes.push(calc_ttime(i, second_time));
-				if (second_time)
+				if (version === 'global'){
+					if (second_time)
+						i++;
+					second_time = !second_time;
+				}
+				else
 					i++;
-				second_time = !second_time;
 			}
 
 			set_notifications();
@@ -60,24 +89,49 @@
 		function calc_ttime(week_num, second_time) {
 			var weekly_order = [0,1,2,3,4];
 			var ttime = my_time.clone();
-			var offset = week_num + my_time.isoWeek() + 3;
+			var offset, i;
 
-			offset = offset % 5;
+			if (version === 'global') {
+				offset = week_num + my_time.isoWeek() + 3;
 
-			for (var i=0; i < offset; i++)
-				weekly_order.unshift(weekly_order.pop());
+				offset = offset % 5;
 
-			for (i=0; i < week_num; i++)
-				ttime.add(1, 'week');
+				for (i=0; i<offset; i++)
+					weekly_order.unshift(weekly_order.pop());
 
-			if (id>4)
-				id -=5;
+				for (i=0; i<week_num; i++)
+					ttime.add(1, 'week');
 
-			ttime.add(weekly_order[id]*2, 'hours');
-			if (second_time)
-				ttime.add(10,'hours');
+				ttime.add(weekly_order[id]*2, 'hours');
+				if (second_time)
+					ttime.add(10,'hours');
+			}
+			else {
+				var day_num = week_num;
+				var monday = jpn_monday;
+				offset = day_num + my_time.isoWeek()*2 + 2;
+				if (!monday)
+					offset += 1;
+				offset = offset % 5;
+				for (i=0; i<offset; i++)
+					weekly_order.unshift(weekly_order.pop());
+
+				for (i=0; i<day_num; i++) {
+					if (monday)
+						ttime.add(4, 'days');
+					else
+						ttime.add(3, 'days');
+					monday = !monday;
+				}
+
+				ttime.add(weekly_order[id]*3, 'hours');
+			}
 
 			return ttime;
+		}
+
+		function set_version(v) {
+			version = v;
 		}
 
 		function get_cal() {
@@ -111,10 +165,8 @@
 
 				time_until = ttimes[i].format('x') - now.format('x');
 				time_until -= notif_time;
-				console.log('time_until = ', time_until);
-				if (time_until < 0)
+				if (time_until < 0 || isNaN(time_until))
 					continue;
-
 				if (notifications[i] !== undefined)
 					clearTimeout(notifications[i]);
 
